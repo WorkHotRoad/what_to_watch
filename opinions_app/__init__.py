15ea226dc1a9f9b953aa4d5b024d823b19844e9d
WorkHotRoad <dima@ezoport.com>
2023-02-21 21:20:50 +0300

    add api_v1

--- opinions_app/__init__.py
+++ opinions_app/__init__.py
@@ -8,7 +8,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from . import cli_commands, error_handlers, views
+from . import api_views, cli_commands, error_handlers, views
 
 # Обратите внимание на расположение импортов в файле: часть из них находится в начале кода, часть в конце.
 # Это сделано намеренно. Дело в том, что подключаемые в самом конце модули опираются в своей работе на те экземпляры классов,
---
+++ opinions_app/api_views.py
@@ -0,0 +1,46 @@
+from flask import jsonify, request
+
+from . import app, db
+from .models import Opinion
+
+
+# Явно разрешить метод GET
+@app.route('/api/opinions/<int:id>/', methods=['GET'])
+def get_opinion(id):
+    # Получить объект по id или выбросить ошибку
+    opinion = Opinion.query.get_or_404(id)
+    # Конвертировать данные в JSON и вернуть объект и код ответа API
+    return jsonify({'opinion': opinion.to_dict()}), 200
+
+
+@app.route('/api/opinions/<int:id>/', methods=['PATCH'])
+def update_opinion(id):
+    data = request.get_json()
+    opinion = Opinion.query.get_or_404(id)
+    # Если метод get_or_404 не найдёт указанный ключ,
+    # то он выбросит исключение 404
+    opinion.title = data.get('title', opinion.title)
+    opinion.text = data.get('text', opinion.text)
+    opinion.source = data.get('source', opinion.source)
+    opinion.added_by = data.get('added_by', opinion.added_by)
+    # Все изменения нужно сохранить в базе данных
+    db.session.commit()
+    # При создании или изменении объекта вернём сам объект и код 201
+    return jsonify({'opinion': opinion.to_dict()}), 201
+
+@app.route('/api/opinions/<int:id>/', methods=['DELETE'])
+def delete_opinion(id):
+    opinion = Opinion.query.get_or_404(id)
+    db.session.delete(opinion)
+    db.session.commit()
+    # При удалении принято возвращать только код ответа 204
+    return '', 204
+
+@app.route('/api/opinions/', methods=['GET'])
+def get_opinions():
+    # Запрашивается список объектов
+    opinions = Opinion.query.all()
+    # Поочерёдно сериализуется каждый объект,
+    # а потом все объекты помещаются в список opinions_list
+    opinions_list = [opinion.to_dict() for opinion in opinions]
+    return jsonify({'opinions': opinions_list}), 200
